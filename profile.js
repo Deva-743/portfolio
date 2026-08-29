2d7fd17eba473547c49f51b0cc0bd14371b885b5
Devarajan A <2007devarajan@gmail.com>
2026-08-29 08:30:34 +0530

    protect the code

--- profile.js
+++ profile.js
@@ -27,3 +27,14 @@ function drop(element)
         element.style.transition = 'transform 0.3s ease';
     }
 }
+// Disable right-click
+document.addEventListener('contextmenu', event => event.preventDefault());
+
+// Disable keyboard shortcuts for developer tools
+document.onkeydown = function(e) {
+  if (e.keyCode == 123) { return false; } // F12
+  if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) { return false; } // Ctrl+Shift+I
+  if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) { return false; } // Ctrl+Shift+C
+  if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) { return false; } // Ctrl+Shift+J
+  if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) { return false; } // Ctrl+U
+}
